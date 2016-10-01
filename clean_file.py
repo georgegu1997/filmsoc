7f3e0827d6ea02c41dd4f8e1805c7d1b74600290
georgegu1997 <283091308@qq.com>
2016-10-01 14:23:34 +0800

    select the file in use (print it out)

--- clean_file.py
+++ clean_file.py
@@ -4,10 +4,61 @@
 # A script to clean the files not in use in the ihome server.
 # The script should be run when you find the storage in the FTP server is almost full.
 
-from datetime import date
-from models import User
+from models import *
 from helpers import delete_file
 
+# Exco -> img_url_id
+# Disk -> cover_url_id
+# Document -> doc_url_id
+# PreviewShowTicket -> cover_url_id
+# Publication -> cover_url_id doc_url_id
+# Sponsor -> img_url_id
+
+def main():
+    '''
+    file_in_use = []
+    file_in_use += Exco.select('img_url_id')
+    file_in_use += Disk.select('cover_url_id')
+    file_in_use += Document.select('doc_url_id')
+    file_in_use += PreviewShowTicket.select('cover_url_id')
+    file_in_use += Publication.select('cover_url_id')
+    file_in_use += Publication.select('doc_url_id')
+    file_in_use += Sponsor.select('img_url_id')
+    '''
+    
+    file_id_in_use = []
+    
+    file_in_use = Exco.select('img_url_id')
+    for file in file_in_use:
+        file_id_in_use.append(file.img_url_id)
+        
+    file_in_use = Disk.select('cover_url_id')
+    for file in file_in_use:
+        file_id_in_use.append(file.cover_url_id)
+        
+    file_in_use = Document.select('doc_url_id')
+    for file in file_in_use:
+        file_id_in_use.append(file.doc_url_id)
+        
+    file_in_use = PreviewShowTicket.select('cover_url_id')
+    for file in file_in_use:
+        file_id_in_use.append(file.cover_url_id)
+        
+    file_in_use = Publication.select('cover_url_id')
+    for file in file_in_use:
+        file_id_in_use.append(file.cover_url_id)
+        
+    file_in_use = Publication.select('doc_url_id')
+    for file in file_in_use:
+        file_id_in_use.append(file.doc_url_id)
+        
+    file_in_use = Sponsor.select('img_url_id')
+    for file in file_in_use:
+        file_id_in_use.append(file.img_url_id)
+        
+    print file_id_in_use
+
+"""
 def main():
     user_sq = User.select().where(
     	User.member_type != 'Expired',
@@ -20,6 +71,6 @@ def main():
     update_mailing_list(
         [x.itsc for x in User.select(User.itsc).where(
             User.member_type != 'Expired')])
-
+"""
 if __name__ == '__main__':
     main()
